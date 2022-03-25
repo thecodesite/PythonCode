887a2e26230ca30193b2e191591bf94d3eb1e691
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2022-03-25 14:21:42 -0400

    Create remove_outlier.py

---
+++ remove_outlier.py
@@ -0,0 +1,11 @@
+#Function to remove outliers
+#Interquartile method
+#Data in ---> pandas dataframe
+def remove_outlier(df_in,col_name):
+    q1 = df_in[col_name].quantile(0.25)
+    q3 = df_in[col_name].quantile(0.75)
+    iqr = q3-q1 #Interquartile range
+    fence_low  = q1-1.5*iqr
+    fence_high = q3+1.5*iqr
+    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
+    return df_out
