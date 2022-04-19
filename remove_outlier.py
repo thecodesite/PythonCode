0e2568093b2e6d0017a7d2e72f1cb47adeca9cde
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2022-04-19 09:44:52 -0400

    Update remove_outlier.py

--- remove_outlier.py
+++ remove_outlier.py
@@ -9,3 +9,57 @@ def remove_outlier(df_in,col_name):
     fence_high = q3+1.5*iqr
     df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
     return df_out
+
+#Helper functions
+def get_iqr_values(df_in, col_name):
+    median = df_in[col_name].median()
+    q1 = df_in[col_name].quantile(0.25) # 25th percentile / 1st quartile
+    q3 = df_in[col_name].quantile(0.75) # 7th percentile / 3rd quartile
+    iqr = q3-q1 #Interquartile range
+    minimum  = q1-1.5*iqr # The minimum value or the |- marker in the box plot
+    maximum = q3+1.5*iqr # The maximum value or the -| marker in the box plot
+    return median, q1, q3, iqr, minimum, maximum
+
+def get_iqr_text(df_in, col_name):
+    median, q1, q3, iqr, minimum, maximum = get_iqr_values(df_in, col_name)
+    text = f"median={median:.2f}, q1={q1:.2f}, q3={q3:.2f}, iqr={iqr:.2f}, minimum={minimum:.2f}, maximum={maximum:.2f}"
+    return text
+
+def remove_outliers(df_in, col_name):
+    _, _, _, _, minimum, maximum = get_iqr_values(df_in, col_name)
+    df_out = df_in.loc[(df_in[col_name] > minimum) & (df_in[col_name] < maximum)]
+    return df_out
+
+def count_outliers(df_in, col_name):
+    _, _, _, _, minimum, maximum = get_iqr_values(df_in, col_name)
+    df_outliers = df_in.loc[(df_in[col_name] <= minimum) | (df_in[col_name] >= maximum)]
+    return df_outliers.shape[0]
+
+def box_and_whisker(df_in, col_name):
+    title = get_iqr_text(df_in, col_name)
+    sns.boxplot(df_in[col_name])
+    plt.title(title)
+    plt.show()
+
+#Remove all outliers (loop)
+def remove_all_outliers(df_in, col_name):
+    loop_count = 0
+    outlier_count = count_outliers(df_in, col_name)
+
+    while outlier_count > 0:
+        loop_count += 1
+
+        if (loop_count > 100):
+            break
+
+        df_in = remove_outliers(df_in, col_name)
+        outlier_count = count_outliers(df_in, col_name)
+    
+    return df_in
+
+# Note that I wouldn't usually iterate through every column in the data frame removing outliers as the data 
+# might not numeric or they might be categorical. The iteration of all columns is just done for expediency here ...
+for column in df:
+    df = remove_all_outliers(df, column)
+    print(f"{column} has {count_outliers(df, column)} outliers")
+    box_and_whisker(df, column)
