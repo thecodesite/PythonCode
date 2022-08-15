958999f8e6c215f437201ae1ca04299edfe089f9
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2022-08-15 11:49:54 -0400

    Create extract_odd_from_list.py

---
+++ extract_odd_from_list.py
@@ -0,0 +1,8 @@
+# We want to extract the position from the list
+
+List = ['Vice President', 'N/A', '4', 'Quantitative Analyst', 'Capital District, Venezuela', '4', 'Talent Acquisition', 'Caracas, Venezuela', '4', 'Founder', 'New York, New York', '4', 'Quantitative Analyst', 
+        'Caracas Metropolitan District, Venezuela', '4', 'Quantitative Researcher', 'New York, New York', '4']
+
+# Using slicing as follow some_list[start:stop:step] we get the odd elements from the list
+
+new_list = list[0::3]
