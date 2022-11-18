12965c117565158137ac48b422e4732f03a8d596
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2022-11-18 08:29:47 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,5 @@
+## How to make a loop that pauses for 5 minutes after each set of 25 iterations until its done?
+for x in 100:
+    #what to do every time (100 times): replace this line with your every-iteration functions.
+    if x % 5 == 0:
+        #what to do every 5th time: replace this line with your nth-iteration functions.
