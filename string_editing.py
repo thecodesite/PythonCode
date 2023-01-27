0f76416ccef8babfeb90c1ac5cd1bc216239c976
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2023-01-27 14:38:28 -0400

    Create string_editing.py

---
+++ string_editing.py
@@ -0,0 +1,19 @@
+txt = "hello world"
+#Capitalize
+x = txt.capitalize()
+print (x) # Hello world
+
+#Uppercase
+x = txt.upper()
+print (x) # HELLO WORLD
+
+#Title
+x = txt.title()
+print (x) # Hello World
+
+txt = "HELLO WORLD"
+#Capitalize
+x = txt.lower()
+print (x) # hello world
+
+
