36897c714d8f3b749090c99861d158e9d7ece97c
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2023-09-05 08:11:58 -0400

    Update selenium_fundamentals.py

--- selenium_fundamentals.py
+++ selenium_fundamentals.py
@@ -73,3 +73,28 @@ browser.get("https://google.com/")
 browser.execute_script("document.title = \'{}\'".format(filename))
 browser.execute_script('window.print();')
 browser.close()
+
+
+#Importing the libraries to get the webdriver
+#pip install wget
+import requests
+import wget
+import zipfile
+import os
+
+# get the latest chrome driver version number
+url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+response = requests.get(url)
+version_number = response.text
+
+# build the donwload url
+download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_mac64.zip"
+
+# download the zip file using the url built above
+latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+
+# extract the zip file
+with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+    zip_ref.extractall("../ChromeDriver_Path") # you can specify the destination folder path here
+# delete the zip file downloaded above
+os.remove(latest_driver_zip)
